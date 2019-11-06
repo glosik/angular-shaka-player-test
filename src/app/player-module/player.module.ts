09a671f49db2aadaf78876aeca801dc358a6b312
glosik <glosik@gmail.com>
2019-11-06 16:57:01 -0500

    feat: add zoom component

--- src/app/player-module/player.module.ts
+++ src/app/player-module/player.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { PlayerComponent } from './player/player.component';
 import { TimelineComponent } from './timeline/timeline.component';
 import { PlayerContainerComponent } from './player-container/player-container.component';
+import { ZoomTimelineComponent } from './zoom-timeline/zoom-timeline.component';
 
+const components = [
+
+];
 @NgModule({
   imports: [
     CommonModule
   ],
-  exports: [ PlayerComponent, TimelineComponent, PlayerContainerComponent ],
-  declarations: [PlayerComponent, TimelineComponent, PlayerContainerComponent ]
+  exports: [ PlayerComponent, TimelineComponent, PlayerContainerComponent, ZoomTimelineComponent ],
+  declarations: [PlayerComponent, TimelineComponent, PlayerContainerComponent, ZoomTimelineComponent ]
 })
 export class PlayerdModule { }
---
+++ src/app/player-module/zoom-timeline/zoom-timeline.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zoom-timeline',
+  templateUrl: './zoom-timeline.component.html',
+  styleUrls: ['./zoom-timeline.component.css']
+})
+export class ZoomTimelineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
