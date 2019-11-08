eb313a5a20e2cd4034ba8e00d9a6bc5855a110ea
glosik <glosik@gmail.com>
2019-11-08 16:26:41 -0500

    feat: hover over timeline

--- src/app/player-module/zoom-timeline/zoom-timeline.component.ts
+++ src/app/player-module/zoom-timeline/zoom-timeline.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, Output, EventEmitter } from "@angular/core";
+import { Component, OnDestroy, AfterViewInit, ViewChild, ElementRef, Output, EventEmitter, NgZone } from "@angular/core";
 import { Subject, fromEvent, Subscription } from "rxjs";
+import * as utilities from '../../shared/utilities';
 
 @Component({
   selector: "app-zoom-timeline",
   templateUrl: "./zoom-timeline.component.html",
   styleUrls: ["./zoom-timeline.component.scss"]
 })
-export class ZoomTimelineComponent implements OnInit, AfterViewInit {
+export class ZoomTimelineComponent implements OnDestroy, AfterViewInit {
   @Output() jumpToFrame = new EventEmitter<number>();
   @ViewChild("canvas") canvas: ElementRef;
   hovering = new Subject();
@@ -18,26 +19,43 @@ export class ZoomTimelineComponent implements OnInit, AfterViewInit {
   private ctxHeight = 100;
   private ctxBgr = 'rgba(0, 0, 200, 0.1)';
   private ctxFgr = 'rgba(0, 0, 200, 0.4)';
-  private duration = 60; // seconds
+  private duration = 60; // seconds // make as @Input
   private canvasLeftOffset;
 
   constructor() { }
 
-  ngOnDestroy() { 
-    this.sub.unsubscribe;
-  }
+ 
 
   ngAfterViewInit() {
     // Capture canvas
     this.ctxEl = this.canvas.nativeElement;
     this.canvasLeftOffset = this.ctxEl.getBoundingClientRect().x;
     this.ctx = this.ctxEl.getContext("2d");
-    
+
+    // Render all hover elements here
     this.sub = fromEvent(this.ctxEl, 'mousemove'
     ).subscribe((e: MouseEvent) => {
-      console.log(Math.round(e.pageX - this.canvasLeftOffset));
+      let boo = Math.round(e.pageX - this.canvasLeftOffset);
+      let sec = Math.round(boo / (this.ctxWidth / this.duration));
+      this.clearCanvas();
+      this.ctx.fillStyle = this.ctxFgr;
+      this.ctx.fillRect(boo, 2, 1, this.ctxHeight);
+      this.ctx.fillStyle = this.ctxBgr;
+      this.ctx.fillRect(boo - 18, 0, 40, 15);
+      this.ctx.fillStyle = "#000";
+      this.ctx.fillText(utilities.makeTime(sec), boo - 18, 10);
+
     });
     // Draw elements
+    this.drawCanvas();
+  }
+
+   ngOnDestroy() {
+    this.sub.unsubscribe;
+  }
+
+  private clearCanvas() {
+    this.ctx.clearRect(0, 0, this.ctxWidth, this.ctxHeight);
     this.drawCanvas();
   }
 
@@ -46,53 +64,54 @@ export class ZoomTimelineComponent implements OnInit, AfterViewInit {
     this.drawGrid(5);
   }
 
-private hover(event){
-  this.hovering.next(true);
-  this.hov = true;
-  console.log('hover', event.x);
-}
-private nothover(){
-   this.hovering.next(false);
-   this.hov = false;
-  console.log('nothover');
-}
+  private hover(event) {
+    this.hovering.next(true);
+    this.hov = true;
+    console.log('hover', event.x);
+  }
+  private nothover() {
+    this.clearCanvas();
+    this.hovering.next(false);
+    this.hov = false;
+    console.log('nothover');
+  }
 
-private ova(event){
-  console.log('ova', event.x);
-}
+  private ova(event) {
+    console.log('ova', event.x);
+  }
 
 
-  private moveToFrame(event: MouseEvent): void {    
+  private moveToFrame(event: MouseEvent): void {
     let canvasMouseX = event.x - this.canvasLeftOffset;
-    let scaledSeconds  = Math.round(canvasMouseX/(this.ctxWidth/this.duration));
-    console.log('click', Math.round(scaledSeconds) );
+    let scaledSeconds = Math.round(canvasMouseX / (this.ctxWidth / this.duration));
+    console.log('click', Math.round(scaledSeconds));
     this.jumpToFrame.emit(scaledSeconds);
   }
 
-  private drawBackdrop():void {
+  private drawBackdrop(): void {
     this.ctx.fillStyle = this.ctxBgr;
     this.ctx.fillRect(0, 0, this.ctxWidth, this.ctxHeight);
   }
 
-  private drawGrid(num:number): void {
+  private drawGrid(num: number): void {
     this.ctx.lineWidth = .5;
     for (var i = 0; i < num; i++) {
       this.ctx.strokeStyle = this.ctxFgr;
       this.ctx.beginPath();
-      this.ctx.moveTo(0 + i * (this.ctxWidth/4), 0);
-      this.ctx.lineTo(0 + i * (this.ctxWidth/4), this.ctxHeight);
+      this.ctx.moveTo(0 + i * (this.ctxWidth / 4), 0);
+      this.ctx.lineTo(0 + i * (this.ctxWidth / 4), this.ctxHeight);
       this.ctx.stroke();
     }
   }
 
-  scaleToWidth(sec){
+  scaleToWidth(sec) {
     // return parseInt(this.ctxWidth/this.props.time_markers.length_seconds*sec,10);
   }
 
-  scaleToWidthChapterZoom(sec, ch_start, ch_end){
+  scaleToWidthChapterZoom(sec, ch_start, ch_end) {
     // return parseInt(this.ctxWidth/(ch_end-ch_start)*(sec-ch_start),10);
   }
 
-   
+
 
 }
